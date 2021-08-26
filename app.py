3bfd7b624301218f3e6a540204e129463c519d1f
Ali Kamel <alik87932@gmail.com>
2021-08-25 23:25:05 -0300

    finished homepage

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ username = file.readline()
 password = file.readline()
 file.close()
 
-app = Flask()
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
 def home():
-    return 'hello'
+    return render_template('index.html')
